f9d290d80d934f7003cd823cf486e0be77907961
Amrit <amritgagra200@gmail.com>
2023-09-28 15:17:37 +0530

    fix some issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,30 @@
+$( ".job__item" ).hover(
+  function() {
+    $('.job__item').addClass('active')
+    $( this ).removeClass( "active" );
+  }, function() {
+    $('.job__item').removeClass('active')
+    $('.job__item').removeClass('active')
+  }
+);
+
+const banner = document.querySelector('.banner');
+const header =  document.querySelector('.header')
+window.onscroll = function() {scrollFunction(banner.offsetHeight)};
+function scrollFunction(scollHeight) {
+  if (document.body.scrollTop > scollHeight  || document.documentElement.scrollTop > scollHeight ) {
+    header.classList.add('active')
+  } else {
+    header.classList.remove('active')
+  }
+}
+
+$(document).ready(function() {
+  if($(window).width() < 993) {
+    window.onscroll = function() {scrollFunction(0)};
+  }
+});
+
 
 if (typeof Swiper !== 'undefined') {
   const blogSwiper = new Swiper('.blog-swiper', {
@@ -6,12 +33,12 @@ if (typeof Swiper !== 'undefined') {
     // loop:true, 
     breakpoints: {
       0: {
-        slidesPerView: 1,
-        spaceBetween: 20,
+        slidesPerView: 1.5,
+        spaceBetween: 50,
       },
       768: {
-        slidesPerView: 2,
-        spaceBetween: 40,
+        slidesPerView: 1.5,
+        spaceBetween: 50,
       },
       1024: {
         slidesPerView: 1.7,
