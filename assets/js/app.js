98bd14f3c3720f75f060f60c07e90d1830c789e2
Amrit <amritgagra200@gmail.com>
2023-10-20 13:40:50 +0530

    fix some issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,41 +31,59 @@ $(document).ready(function() {
   $('.callback-link').on('click',function(){
     $('.callback-popup').toggle();
     $('.callback-popup').toggleClass('fade');
+    $('body').addClass("overflow-hidden");
+    $(document).on('click', function(e){
+      if(!$(e.target).closest(".callback-wrapper").length ) {
+        $('.callback-popup').hide();
+        $('body').removeClass("overflow-hidden");
+      }
+    })
   })
   $('.popup-close-btn').on('click', function(){
     $('.callback-popup').hide();
+    $('body').removeClass("overflow-hidden");
   })
   
-  $(document).on('click', function(e){
-    if(!$(e.target).closest(".callback-wrapper").length) {
-      $('.callback-popup').hide();
+
+  
+  $('.offer-link').on('click',function(){
+    $('.offer-wrapper').toggle();
+    $('.offer-wrapper').toggleClass('fade');
+    $('body').addClass("overflow-hidden");
+    $(document).on('click', function(e){
+    if($(e.target).closest(".offer-wrapper").length) {
+      console.log('click');
+        $('.offer-wrapper').hide();
+        $('body').removeClass("overflow-hidden");
     }
+  })
+  })
+  $(' .offer-wrapper  .popup-close-btn').on('click', function(){
+    $('.offer-wrapper').hide();
+    $('body').removeClass("overflow-hidden");
   })
 });
 
-let target = document.querySelector(".feature");
-let observer = new IntersectionObserver(entries => {
-  function counter(id, start, end, duration) {
-    let obj = document.querySelector(id),
-     current = start,
-     range = end - start,
-     increment = end > start ? 1 : -1,
-     step = Math.abs(Math.floor(duration / range)),
-     timer = setInterval(() => {
-      current += increment;
-      obj.textContent = current;
-      if (current == end) {
-       clearInterval(timer);
+let faces = document.querySelector('.faces')
+if(faces) {
+  gsap.registerPlugin(ScrollTrigger)
+  const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.faces',
+        start: '300px top',
+        end: 'center top',
+        scrub: 1,
+        ease: 'linear',
       }
-     }, step);
-   }
-counter(".counter1", 0, 10, 3000);
-  counter(".counter2", 0, 100, 3000);
-  counter(".counter3", 0, 500, 3000);
-});
-
-observer.observe(target);
-
+  })
+  gsap.set('.scroll-text',{
+    x:700,
+  })
+  tl.to('.scroll-text', {
+   duration:2,
+    x:-1200,
+  })
+}
 
 
 if (typeof Swiper !== 'undefined') {
@@ -99,24 +117,35 @@ if (typeof Swiper !== 'undefined') {
   })
 }
 
-gsap.registerPlugin(ScrollTrigger)
+let target = document.querySelector(".feature");
 
-const tl = gsap.timeline({
-  scrollTrigger: {
-      trigger: '.faces',
-      start: '300px top',
-      end: 'center top',
-      scrub: 1,
-      ease: 'linear',
-    }
-})
-gsap.set('.scroll-text',{
-  x:700,
-})
-tl.to('.scroll-text', {
- duration:2,
-  x:-1200,
-})
+let observer = new IntersectionObserver(entries => {
+  function counter(id, start, end, duration) {
+    let obj = document.querySelector(id),
+     current = start,
+     range = end - start,
+     increment = end > start ? 1 : -1,
+     step = Math.abs(Math.floor(duration / range)),
+     timer = setInterval(() => {
+      current += increment;
+      obj.textContent = current;
+      if (current == end) {
+       clearInterval(timer);
+      }
+     }, step);
+   }
+counter(".counter1", 0, 10, 3000);
+  counter(".counter2", 0, 100, 3000);
+  counter(".counter3", 0, 500, 3000);
+});
+if(target) {
+  observer.observe(target);
+}
+
+
+
+
+
 
 
 
